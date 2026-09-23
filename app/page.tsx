06ca39139fa9a94fbf8f0e9dfdaae5b7f62e6d7e
ellietownsend <epaigetownsend@gmail.com>
2026-09-23 13:59:57 -0700

    Update Home component with portfolio details

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+'use client';
+import Project from "./_components/project";
+
+
 
 export default function Home() {
   return (
-   <main><h1>Welcome to the Home Page</h1></main>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
+      <h1>Welcome to Ellie's Portfolio</h1>
+      <Project
+        imagePath="/girls-who-math-dashboard.png"
+        imageLogoPath="/db-logo.png"
+        projectName="Daily Bruin"
+        gitHubLink="https://github.com/dailybruin/buzz"
+        linkedIn="https://www.linkedin.com/in/ellietownsend"
+      />
+    </div>
   );
 }
